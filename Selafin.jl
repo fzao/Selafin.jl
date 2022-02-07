bf65641e5dae3e4fd9bb334fa99af5825cafd1c3
Fabrice ZAOUI <fabrice.zaoui@edf.fr>
2022-02-07 16:50:59 +0100

    first plot of variable values

--- Selafin.jl
+++ Selafin.jl
@@ -17,17 +17,22 @@ using .Model
 
 # Selafin file
 #filename = "malpasset.slf"
-#filename = "mersey.slf"
+filename = "mersey.slf"
 #filename = "Alderney_sea_level.slf"
 #filename = "Alderney.slf"
 #filename = "girxl2d_result.slf"
-filename = "a9.slf"
+#filename = "a9.slf"
+#filename = "Q_Usine.slf"
 
 # read file
 data = Read(filename);
 
 # mesh quality
-# qual = Quality(data, true);
+#qual = Quality(data, true);
 
 # variable values
 val = Get(data, 3, 1)
+f = Figure()
+Axis(f[1, 1], title="Water depth (m)")
+scatter!(data.x, data.y, color=val)
+f
