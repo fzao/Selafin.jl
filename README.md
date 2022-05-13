e3bddcefa121ce05f9078f3810fc98c1a627b3c1
Fabrice ZAOUI <fabrice.zaoui@edf.fr>
2022-05-13 12:55:12 +0200

    3D results OK

--- README.md
+++ README.md
@@ -3,37 +3,110 @@ Telemac Selafin file reader in Julia
 
 This program is a reader and viewer of the Telemac Selafin file (www.opentelemac.org) in the Julia programming language.
 
-Purpose: get info on the Telemac result file (values and mesh)
+Purpose: get info on the Telemac result file (2D/3D values and 2D mesh)
 
-Use
-===
+## Telemac 2D example
 
-Three user's functions are defined to read the Selafin file, get info on the mesh quality, and get results at the desired time info.
+Three user's functions are defined in order to read the Selafin file, get info on the mesh quality, and get results at the desired time info and layer.
 
-The following code is an example use on a Telemac 2D file:
-
-<p align="left">
-  <img src="./fig/example.png" width="300"/>
-</p>
-
-
-The latter will give the following print out:
+The following code is an example use on a Telemac 2D file. The first step is to read the Selafin file with the *Read()* function:
 
 <p align="left">
-  <img src="./fig/printout.png" width="600"/>
+  <img src="./fig/t2d_example1.png" width="350"/>
 </p>
 
-and the following figures for the mesh quality:
+This will display the following information on the file:
+
+<p align="left">
+  <img src="./fig/t2d_display1.png" width="800"/>
+</p>
+
+And in return, the user will have access to a Julia data structure:
+
+<p align="left">
+  <img src="./fig/t2d_display2.png" width="800"/>
+</p>
+
+The mesh can be analyzed with the *Mesh()* function by giving the data structure:
+
+<p align="left">
+  <img src="./fig/t2d_display3.png" width="200"/>
+</p>
+
+The mesh quality is then displayed with the following histogram:
+
+<p align="left">
+  <img src="./fig/t2d_display4.png" width="150"/>
+</p>
+
+The mesh can also be viewed by adding the logical parameter:
+
+<p align="left">
+  <img src="./fig/t2d_display5.png" width="300"/>
+</p>
 
 <p align="center">
-  <img src="./fig/meshvis.png" width="800"/>
+  <img src="./fig/t2d_display6.png" width="800"/>
 </p>
 
- and for the 2D results of the water depth:
+To get the results on the water depth, the *Get()* function has to be used. Here, the water depth is the variable number 3. As only one time step is available, the call will be:
+
+<p align="left">
+  <img src="./fig/t2d_display7.png" width="600"/>
+</p>
+
+In return, the user will have access to a vector (1D real array):
+
+<p align="left">
+  <img src="./fig/t2d_display8.png" width="250"/>
+</p>
+
+The result can also displayed and saved as an image by adding the logical parameter and a file name (png format required):
+
+<p align="left">
+  <img src="./fig/t2d_display9.png" width="500"/>
+</p>
 
 <p align="center">
-  <img src="./fig/resvis.png" width="800"/>
+  <img src="./fig/myFirstResult.png" width="800"/>
 </p>
+
+## Telemac 3D example
+
+The logic is the same. Firstly, we will read the case:
+
+<p align="left">
+  <img src="./fig/t3d_example1.png" width="350"/>
+</p>
+
+This will display the following information on the file:
+
+<p align="left">
+  <img src="./fig/t3d_display1.png" width="550"/>
+</p>
+
+This case has 4 variables, 9 time steps and 6 layers.
+
+The mesh can also be analyzed and viewed by adding the logical parameter:
+
+<p align="left">
+  <img src="./fig/t2d_display5.png" width="300"/>
+</p>
+
+<p align="center">
+  <img src="./fig/t3d_display2.png" width="800"/>
+</p>
+
+Finally, in order to get and see the vertical velocity (*w*), at the time step number 5 and for the layer no. 6, the function call will be:
+
+<p align="left">
+  <img src="./fig/t3d_display3.png" width="300"/>
+</p>
+
+<p align="center">
+  <img src="./fig/t3d_display4.png" width="800"/>
+</p>
+
 
 Requirements
 ============
--- Variables.jl
+++ Variables.jl
@@ -1,9 +1,9 @@
 # Variables.jl : this file is a part of the Selafin.jl project (a reader and viewer of the Telemac Selafin file (www.opentelemac.org) in the Julia programming language)
 #
-# Read an array of Telemac results based on a variable number and time step number
+# Read an array of Telemac results based on a variable number, a time step number and a number of layer (T3D)
 # Optionally displays (figopt parameter) and prints out (figname parameter) the results
 # Return the real values if succeed
-# 
+#
 # Released under the MIT License
 #
 # Copyright (c) 2021 Fabrice Zaoui
@@ -18,7 +18,7 @@
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 #
-function Get(data, novar=0, notime=0, figopt=false, figname=nothing)
+function Get(data, novar=0, notime=0, noplane=1, figopt=false, figname=nothing)
 
     if typeof(data) != Data
         println("$(Parameters.noksymbol) Parameter is not a Data struct")
@@ -38,15 +38,19 @@ function Get(data, novar=0, notime=0, figopt=false, figname=nothing)
         println("$(Parameters.noksymbol) The time number exceeds the number of records")
         return
     end
-    if data.nblayers > 1
-        println("$(Parameters.noksymbol) 3D variables not yet implemented...")
+    if noplane < 1
+        println("$(Parameters.noksymbol) The layer number is not positive")
+        return
+    end
+    if noplane > data.nblayers
+        println("$(Parameters.noksymbol) The layer number exceeds the max value")
         return
     end
 
     fid = open(data.filename, "r")
     seek(fid, data.markposition)
 
-    variables = Array{data.typefloat, 1}(undef, data.nbnodes)
+    variables = Array{data.typefloat, 1}(undef, data.nbnodesLayer * data.nblayers)
     timevalue =  Array{Float32, 1}(undef, data.nbsteps)
     exitloop = false
     for t in 1:data.nbsteps
@@ -72,13 +76,17 @@ function Get(data, novar=0, notime=0, figopt=false, figname=nothing)
     # close the Selafin file
     close(fid)
 
+    # reshape and extract
+    values = reshape(variables, data.nbnodesLayer, data.nblayers)
+    variables = values[:, noplane]
+
     # statistics
     minval = round(minimum(variables), digits = 2)
     meanval = round(mean(variables), digits = 2)
     medval = round(median(variables), digits = 2)
     maxval = round(maximum(variables), digits = 2)
     varname = lstrip(rstrip(data.varnames[novar]))
-    timesec = (notime - 1) * data.timestep 
+    timesec = (notime - 1) * data.timestep
     println("$(Parameters.oksymbol) Read variable #$(novar) ($(varname)) at time record #$(notime) ($(timesec) s) with:")
     println("\t Min. value: $minval")
     println("\t Max. value: $maxval")
@@ -87,9 +95,14 @@ function Get(data, novar=0, notime=0, figopt=false, figname=nothing)
 
     if figopt
         fig = Figure()
-        Axis(fig[1, 1], title=data.varnames[novar], xlabel = "x-coordinates (m)", ylabel = "y-coordinates (m)")
-        Colorbar(fig[1, 2], limits = (minimum(variables), maximum(variables)), colormap = :viridis)
-        mesh!([data.x data.y], data.ikle, color=variables, colormap=:viridis, shading=false)
+        Axis(fig[1, 1], title=data.varnames[novar]*" TIME_STEP($(notime)) "*" NB_LAYER($(noplane)) ", xlabel = "x-coordinates (m)", ylabel = "y-coordinates (m)")
+        maxvar = maximum(variables)
+        minvar = minimum(variables)
+        if minvar == maxvar
+            maxvar = minvar + Parameters.eps
+        end
+        Colorbar(fig[1, 2], limits = (minvar, maxvar), colormap = :viridis)
+        mesh!([data.x[1:data.nbnodesLayer] data.y[1:data.nbnodesLayer]], data.ikle[1:data.nbtrianglesLayer, 1:3], color=variables, colormap=:viridis, shading=false)
 
         if !isnothing(figname)
             save(figname, fig, px_per_unit = 2)
