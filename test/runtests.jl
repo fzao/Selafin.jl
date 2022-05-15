81f9bdfa253a0dc7fff64a8098078f92753272ed
Fabrice ZAOUI <fabrice.zaoui@edf.fr>
2022-05-15 12:20:09 +0200

    syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ using Selafin
     @test isapprox((Selafin.Read("../data/t2d_malpasset.slf")).x[1], 5905.615234375)
     @test isapprox((Selafin.Read("../data/t2d_malpasset.slf")).y[13541], 4427.7314453125)
     @test min(Selafin.Quality((Selafin.Read("../data/t2d_malpasset.slf")))...) > 0.12
-    @test Selafin.Get(Selafin.Read("../data/t2d_malpasset.slf"), 2) == nothing
+    @test isnothing(Selafin.Get(Selafin.Read("../data/t2d_malpasset.slf"), 2))
     @test isapprox(min(Selafin.Get(Selafin.Read("../data/t2d_malpasset.slf"), 3, 1)...), 0.)
     @test size(Selafin.Get(Selafin.Read("../data/t2d_malpasset.slf"), 5, 1))[1] == 13541
     @test (Selafin.Read("../data/t3d_piledepon.slf")).nbnodesLayer == 2280
@@ -20,6 +20,6 @@ using Selafin
     @test min(Selafin.Quality((Selafin.Read("../data/t3d_piledepon.slf")))...) > 0.47
     @test isapprox(max(Selafin.Quality((Selafin.Read("../data/t3d_piledepon.slf")))...), 0.9931093f0)
     @test all(Selafin.Get(Selafin.Read("../data/t3d_piledepon.slf"),1 ,1 ,1) .< 0)
-    @test Selafin.Get(Selafin.Read("../data/t3d_piledepon.slf"),1 ,1 ,10) == nothing
+    @test isnothing(Selafin.Get(Selafin.Read("../data/t3d_piledepon.slf"),1 ,1 ,10))
     @test isapprox(max(Selafin.Get(Selafin.Read("../data/t3d_piledepon.slf"),2, 3, 6)...), 2.4606469f0)
 end
